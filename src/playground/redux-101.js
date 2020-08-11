b0174314ad9cd5f31a3c40f75a52a443905eedfd
Aleksander <aleksandergorecki80@gmail.com>
2020-08-11 17:27:44 +0100

    Dispatching Actions

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,8 +1,25 @@
 import { createStore } from 'redux';
 
 const store = createStore((state = { count: 0 }, action) => {
-  console.log('store is running')
-  return state;
+  switch(action.type){
+    case 'INCREMENT':
+      return {
+        count: state.count + 1
+      }
+    break;
+    case 'DECREMENT':
+      return {
+        count: state.count - 1
+      }
+    break;
+    case 'RESET':
+      return {
+        count: 0
+      }
+    break;
+    default:
+      return state;
+  }
 });
 
 
@@ -18,8 +35,26 @@ const increment = {
   type: 'INCREMENT'
 }
 
-store.dispatch(increment);
+const decrement = {
+  type: 'DECREMENT'
+}
 
+const reset = {
+  type: 'RESET'
+}
+
+store.dispatch(increment);
 console.log(store.getState());
 
+store.dispatch(decrement);
+console.log(store.getState());
+
+store.dispatch(decrement);
+console.log(store.getState());
+
+
+
 // I'd like to reset the count to zero
+
+store.dispatch(reset);
+console.log(store.getState());
