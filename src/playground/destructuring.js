87fc7a22c16747f4e9c3e2242ca39590a939b143
Aleksander <aleksandergorecki80@gmail.com>
2020-08-12 17:08:46 +0100

    ES6 Array Destructuring

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -11,3 +11,8 @@ const book = {
 const { name: PublisherName = 'Self Published' } = book.publisher;
 
 console.log(PublisherName); // Self-Publish
+
+
+const item = ['Coffe (hot)', '$2.00', '$2.50', '2.75'];
+const [ name, , medium ] = item;
+console.log(`A medium ${name} cost ${medium}`)
